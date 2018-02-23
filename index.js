ba4b296e2c36b78de2106ae8daf359566bd259dd
zmahfuz817 <zunayed.lm@gmail.com>
2018-02-23 18:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function shout (string){
 function whisper(string) {
   return string.toLowerCase()
 }
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase //true
+
+var lowercase = "hello!"
+lowercase.toLowerCase() === lowercase //true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase //false
+mixedCase.toUpperCase() === mixedCase //false
